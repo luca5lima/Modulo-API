620b9802a9370d85d38bc429eb1efc3fdfab14e9
Lucas Lima Cavalcante <lucaslimoa2000@gmail.com>
2025-11-28 20:39:22 -0300

    Criando ContatoController

---
+++ Controllers/ContatoController.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Modulo_API.Entities;
+using Modulo_API.Context;
+
+namespace Modulo_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ContatoController : ControllerBase
+    {
+        private readonly AgendaContext _context;
+
+        public ContatoController(AgendaContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public IActionResult Create(Contato contato)
+        {
+            _context.Add(contato);
+            _context.SaveChanges();
+            return Ok(contato);    
+        }
+    }
+}
